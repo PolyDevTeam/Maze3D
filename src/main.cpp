036a2879a6931bf7488c5e5a2a516a6a26a83487
slydevis <slydevis@hotmail.fr>
2018-09-13 09:38:54 +0200

    Add library test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#ifdef __linux__
+    #include <GL/gl.h>
+#elif __APPLE__
+    #include <OpenGL/gl.h>
+#endif
+
+int main(int argc, char** argv) {
+    // OpenGL test
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // OpenCV test
+    cv::Mat images;
+
     std::cout << "Hello World" << std::endl;
     return 0;
 }
