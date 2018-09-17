bd73a752683eac58d8486c6d71d796cebd12d74e
Stamegna <clement.stamegna@hotmail.fr>
2018-09-17 08:23:08 +0200

    [cmakeList] use find package to find opencv includes and libs [main.cpp] add good includes for win32 and test imshow and glfw open window

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #elif defined(_WIN32)
 	#include <GLFW/glfw3.h>
 	#include <gl/GL.h>
+	#include <opencv2/core/opengl.hpp>
 #else
 	#include <GL/gl.h>
 #endif
@@ -20,8 +21,54 @@ int main(int argc, char** argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // OpenCV test
-    cv::Mat images;
+    cv::Mat img;
 
     std::cout << "Hello World" << std::endl;
-    return 0;
+
+
+	img = cv::imread("MAZEtte.png", cv::IMREAD_COLOR); // Read the file
+	if (img.empty()) // Check for invalid input
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+
+
+	cv::namedWindow("Hello World");
+	cv::imshow("Hello World", img); // Show our image inside it.
+
+	cv::waitKey(0); // Wait for a keystroke in the window
+
+
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
 }
