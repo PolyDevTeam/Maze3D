c2098f33e7b7befe42ab01bcfa19fad0dc4d82d6
DarkPlouc <33123645+DarkPlouc@users.noreply.github.com>
2018-09-21 11:34:17 +0200

    Ajout initialisation + amélioration homographie

--- src/Game.cpp
+++ src/Game.cpp
@@ -51,7 +51,7 @@ void Game::start(int argc, char **argv) {
 	std::vector<Point2i> objRef2;
 	Mat h;
 
-	int eps = 30;
+	int eps = 100;
 
     // Get WebCam
     VideoCapture capture(0);
@@ -91,16 +91,137 @@ void Game::start(int argc, char **argv) {
 	p3 = Point2i(0, 3);
 	p4 = Point2i(0, 4);
 
-	bool bool1;
-	bool bool2;
-	bool bool3;
-	bool bool4;
+	bool initialisation;
+
+	bool bool1 = false;
+	bool bool2 = false;
+	bool bool3 = false;
+	bool bool4 = false;
+	int compteur = 0;
 
 
-	objCapture.push_back(p1);
-	objCapture.push_back(p2);
-	objCapture.push_back(p3);
-	objCapture.push_back(p4);
+	initialisation = false;
+	while (cvWaitKey(10) != 'q' && initialisation == false) {
+
+		//initialisation
+		capture >> src;
+		
+
+		// Convert to grayscale
+		cv::cvtColor(src, gray, CV_BGR2GRAY);
+
+		// Use Canny instead of threshold to catch squares with gradient shading
+		blur(gray, bw, Size(3, 3));
+		cv::Canny(gray, bw, 80, 240, 3);
+
+		// Display canny image
+		cv::imshow("bw", bw);
+
+		// Find contours
+		cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+		src.copyTo(dst);
+
+		bool bool1 = false;
+		bool bool2 = false;
+		bool bool3 = false;
+		bool bool4 = false;
+
+		for (auto &contour : contours) {
+
+			// Approximate contour with accuracy proportional
+			// to the contour perimeter
+			cv::approxPolyDP(cv::Mat(contour), approx, cv::arcLength(cv::Mat(contour), true) * 0.02, true);
+
+			// Skip small or non-convex objects
+			if (std::fabs(cv::contourArea(contour)) < 100 || !cv::isContourConvex(approx)) {
+				continue;
+			}
+
+			if (approx.size() == 3) {
+				Image::setLabel(dst, "TRI", contour);  // Triangles
+				r = boundingRect(contour);
+
+				//if ((r.x > p3.x - eps && r.x<p3.x + eps && r.y>p3.y - eps && r.y < p3.y + eps) || p3.x == 0) {
+				p3.x = r.x;
+				p3.y = r.y;
+				bool3 = true;
+				//}
+			}
+			else if (approx.size() >= 4 && approx.size() <= 6) {
+				// Number of vertices of polygonal curve
+				int vtc = static_cast<int>(approx.size());
+
+				// Use the degrees obtained above and the number of vertices
+				// to determine the shape of the contour
+				if (vtc == 4) {
+					Image::setLabel(dst, "RECKT", contour);
+					r = boundingRect(contour);
+					//if ((r.x > p1.x - eps && r.x<p1.x + eps && r.y>p1.y - eps && r.y < p1.y + eps) || p1.x == 0) {
+					p1.x = r.x;
+					p1.y = r.y;
+					bool1 = true;
+
+					//}
+				}
+				else if (vtc == 5) {
+					Image::setLabel(dst, "PENTA", contour);
+				}
+				else if (vtc == 6) {
+					Image::setLabel(dst, "HEXA", contour);
+					r = boundingRect(contour);
+					//if ((r.x > p2.x - eps && r.x<p2.x + eps && r.y>p2.y - eps && r.y < p2.y + eps) || p2.x == 0) {
+					p2.x = r.x;
+					p2.y = r.y;
+					bool2 = true;
+
+					//}
+				}
+			}
+			else {
+				// Detect and label circles
+				double area = cv::contourArea(contour);
+				cv::Rect r = cv::boundingRect(contour);
+				int radius = r.width / 2;
+
+				if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
+					std::abs(1 - (area / (CV_PI * (radius * radius)))) <= 0.2)
+					Image::setLabel(dst, "CIR", contour);
+				r = boundingRect(contour);
+				//if ((r.x > p4.x - eps && r.x<p4.x + eps && r.y>p4.y - eps && r.y < p4.y + eps) || p4.x==0) {
+				p4.x = r.x;
+				p4.y = r.y;
+				bool4 = true;
+
+				// }
+			}
+		}
+
+		// Draw game
+		cv::imshow("dst", dst);
+
+		if (bool1 && bool2 && bool3 &bool4) {
+			
+			p11 = p1;
+			p12 = p2;
+			p13 = p3;
+			p14 = p4;
+
+			initialisation = true;
+			cout<<"FIN INITIALISATION"<<endl;
+		}
+	}
+
+
+
+	
+
+
+
+
+
+
+
 
     // Loop
     while (cvWaitKey(10) != 'q') {
@@ -112,7 +233,6 @@ void Game::start(int argc, char **argv) {
         cv::cvtColor(src, gray, CV_BGR2GRAY);
 
         // Use Canny instead of threshold to catch squares with gradient shading
-
         blur(gray, bw, Size(3, 3));
         cv::Canny(gray, bw, 80, 240, 3);
 
@@ -205,34 +325,51 @@ void Game::start(int argc, char **argv) {
 		cv::imshow("dst", dst);
 
 		if (bool1 && bool2 && bool3 &bool4) {
-			p11 = p1;
-			p12 = p2;
-			p13 = p3;
-			p14 = p4;
+			
 
-			objCapture.clear();
-			objCapture.push_back(p11);
-			objCapture.push_back(p12);
-			objCapture.push_back(p13);
-			objCapture.push_back(p14);
+
+
+
+			if (p1.x > p11.x - eps && p1.x<p11.x + eps && p1.y>p11.y - eps && p1.y < p11.y + eps &&
+				p2.x>p12.x - eps && p2.x<p12.x + eps && p2.y>p12.y - eps && p2.y < p12.y + eps &&
+				p3.x>p13.x - eps && p3.x<p13.x + eps && p3.y>p13.y - eps && p3.y < p13.y + eps &&
+				p4.x>p14.x - eps && p4.x<p14.x + eps && p4.y>p14.y - eps && p4.y < p14.y + eps) {
+				compteur++;
+			}
+			else compteur = 0;
+			if (compteur == 2) {
+				p11 = p1;
+				p12 = p2;
+				p13 = p3;
+				p14 = p4;
+
+				objCapture.clear();
+				objCapture.push_back(p11);
+				objCapture.push_back(p12);
+				objCapture.push_back(p13);
+				objCapture.push_back(p14);
+				compteur = 0;
+			}
 		}
 
-		
-		h = findHomography(objCapture, objRef, RANSAC);
-		
-		
-		//cout << h<<endl<<endl;
-
-		
-		try {
-			warpPerspective(toad, final, h, final.size());
-		}
-		catch (std::exception const& e)
+		if (objCapture.size() == 4)
 		{
-			cerr << "ERREUR : " << e.what() << endl;
-		}
+			h = findHomography(objCapture, objRef, RANSAC);
 
-		cv::imshow("final", final);
+
+			//cout << h << endl << endl;
+
+
+			try {
+				warpPerspective(toad, final, h, final.size());
+			}
+			catch (std::exception const& e)
+			{
+				cerr << "ERREUR : " << e.what() << endl;
+			}
+
+			cv::imshow("final", final);
+		}
     }
 
     // Stop the game
